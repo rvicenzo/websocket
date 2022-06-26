a8fd8f7a1968c0b3cb6bbd644499b6b574b2260b
Rafael <rafael.silva@luizalabs.com>
2022-06-26 15:55:00 -0300

    io cors

--- src/http.ts
+++ src/http.ts
@@ -3,8 +3,14 @@ import http from "http";
 import { Server } from "socket.io";
 
 const app = express();
+
 const serverHttp = http.createServer(app);
-const io = new Server(serverHttp);
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+});
 
 export {
     serverHttp,
